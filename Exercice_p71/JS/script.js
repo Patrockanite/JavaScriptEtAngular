7011eb3f2b0d72353ebf73b02cd99e9a1ec50a09
Patrockanite <patrickannoot@orange.fr>
2021-07-02 21:11:47 +0200

    exercices page 71 OK

--- Exercice_p71/JS/script.js
+++ Exercice_p71/JS/script.js
@@ -12,22 +12,62 @@ document.addEventListener('DOMContentLoaded',function(event){
     para1.innerHTML=suite;
 
     let titre1 = document.createElement('h2');
-    titre1.innerHTML="Les nombres de 0 à 200";
+    titre1.innerHTML="Les nombres de 0 à 200 :";
     contenu.appendChild(titre1);
     contenu.appendChild(para1);
 
-    /***************** Execrice 2 suite de fibo de 1 à 666  */
+    /***************** Execrice 2 suite de fibonacci de 1 à 666  ************/
+    /************* chaque valeur est la somme des 2 précédentes ***********/
 
     let para2 = document.createElement('p');
+    let fibo = [];
+    let suivant = 0;
+    let a = 1;
+    let b = 1;
+    while(suivant<666){
+        if(suivant == 0){
+            fibo.push(a);
+            fibo.push(b);
+            suivant = a+b;
+        }
+        else{
+            fibo.push(suivant);
+            a = b ;
+            b = suivant ;
+            suivant = a + b;           
+           
+        }
+        //console.log(fibo);
+       
+    }    
     
-    
-
-    para2.innerHTML=fibo;
+    for(i=0;i<fibo.length;i++){
+        para2.innerHTML += (fibo[i]).toString()+" ; ";
+    }
 
     let titre2 = document.createElement('h2');
-    titre1.innerHTML="Suite de Fibo";
+    titre2.innerHTML="Suite de Fibonacci à moins de 666 :";
     contenu.appendChild(titre2);
     contenu.appendChild(para2);
+
+    /****************** Exrcice 3 nombres premiers de 1 à 61*******************/
+    let titre3 = document.createElement('h2');
+    titre3.innerHTML = "Nombres premiers de 1 à 61 :";
+    contenu.appendChild(titre3);
+    let para3= document.createElement('p');
+
+    let NbPremiers = "";
+    for(i=1;i<=61;i++){
+        if(i%2 !=0){
+            NbPremiers += i.toString()+" ; ";
+        }
+    }
+    para3.innerHTML = NbPremiers;
+    contenu.appendChild(para3);
+
+
+
+
 
 
 });
