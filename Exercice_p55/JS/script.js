26bd5f798d232db58a65c79b2a53751f01618924
Patrockanite <patrickannoot@orange.fr>
2021-07-02 09:03:41 +0200

    initialisation

---
+++ Exercice_p55/JS/script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded",function(event){
+    let tableau =['a','b',6,'d',3,'b',99,'e',6,'e','c',15,'f',8,'b',6,'f',5,'b',338576,'f',057,'e'];
+    document.writeln(tableau[0]+"<br>");
+    document.writeln(tableau[2]+tableau[4]+"<br>");
+    let alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+    document.writeln("l'index est : " + alphabet.indexOf(alphabet[7]));
+
+
+
+
+
+
+});
