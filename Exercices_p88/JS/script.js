660ade9a3fda334223746d63aa31f5d2c8e96325
Patrockanite <patrickannoot@orange.fr>
2021-07-03 22:28:26 +0200

    fin des exercices P88

--- Exercices_p88/JS/script.js
+++ Exercices_p88/JS/script.js
@@ -5,30 +5,62 @@ document.addEventListener('DOMContentLoaded',function(event){
     let contenu = document.getElementById("main-content");
     let para1 = document.createElement('p');
     let para2 = document.createElement('p');
-    let para3 = document.createElement('p');
+    /************************** test 2 nombres **********************/
+    alert("On commence pas 2 chiffres");
+    let a = prompt("Entrez un nombre entier");
+    let b = prompt("Entrez un nombre entier");
 
-    para1.innerHTML=maxDe2Nombres(3,21);
-    para2.innerHTML=maxDe2Nombres(12,6);
-    para3.innerHTML=maxDe2Nombres(36,36);
+   
+    if(a===b){
+        para1.innerHTML = "Les entrées sont égales !";
+    }
+    else{
+        a = maxDe2Nombres(parseInt(a,10),parseInt(b,10));
+        para1.innerHTML = a + " est le nombre le plus grand";
+    }
+    contenu.appendChild(para1);
 
+    /********************* Test 3 nombres ************************/
+    alert("maitenant on en entre 3 ");
+    a = prompt("Entrez un nombre entier");
+    b = prompt("Entrez un nombre entier");
+    let c = prompt("Entrez un nombre entier");
+
+   
+    if(a==b&&b==c){
+        para2.innerHTML = "Les entrées sont égales !";
+    }
+    else{
+        a = maxDe3Nombres(parseInt(a,10),parseInt(b,10),parseInt(c,10));
+        para2.innerHTML = a + " est le nombre le plus grand";
+    }
+    contenu.appendChild(para2);
     
-
-    function maxDe2Nombres(a,b){
-        if(a && b){
-            if (a>b){
-                return(a + " est supérieur à " + b + "<br>"); //ne pas utiliser document.write() qui écrase ce qui est déjà sur le document !!!
+    
+/****************************** fonctions **************************/
+   
+    function maxDe2Nombres (a,b){
+        if(!isNaN(a) && !isNaN(b)){
+            if(a>b){
+                return a;
+            }
+            else if(b>a){
+                return b;
             }
             else{
-                return(b>a ? b + " est supérieur à " + a + "<br>" : a + " et " + b + " sont égaux<br>");
+                return a;
             }
         }
         else{
             alert("Vous n'avez pas entré les nombres correctement");
         }
     }
+
+    function maxDe3Nombres (a,b,c){
+        let d = maxDe2Nombres(a,b);
+        return maxDe2Nombres(c,d);
+    }
     
-    contenu.appendChild(para1);
-    contenu.appendChild(para2);
-    contenu.appendChild(para3);
+    
 
 });
